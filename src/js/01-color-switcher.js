fb2eb8eab0b53452ae785b6ebaaeee58b7d52f9c
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-09-09 20:58:57 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,39 +1,41 @@
-// // ! Напиши скрипт, який після натискання кнопки «Start»,
-// // ! раз на секунду змінює колір фону <body> на випадкове
-// // ! значення, використовуючи інлайн стиль.
-// // !УВАГА
-// // !Враховуй, що на кнопку «Start» можна натиснути
-// // !нескінченну кількість разів.
-// // !Зроби так, щоб доки зміна теми запущена,
-// // !кнопка «Start» була неактивною (disabled).
-// // !Натисканням на
-// // !кнопку «Stop» зміна кольору фону повинна зупинятися.
 
-// function getRandomHexColor() {
-//     return #${Math.floor(Math.random() * 16777215).toString(16)};
-//   }
-//   let timerId = null;
+// Напиши скрипт, який після натискання кнопки «Start», 
+// раз на секунду змінює колір фону <body> на випадкове значення, використовуючи інлайн стиль. 
+// Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.
+
+// !УВАГА
+// Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. 
+// Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною (disabled).
+
+// Для генерування випадкового кольору використовуй функцію getRandomHexColor.
+
+
+  const buttonStart = document.querySelector('button[data-start]');
+  const buttonStop = document.querySelector('button[data-stop]');
+//   console.log(buttonStart, buttonStop);
+   
+  buttonStart.addEventListener('click', onStartClick);
+  buttonStop.addEventListener('click', onStopClick);
   
-//   const btnStart = document.querySelector('button[data-start]');
-//   const btnStop = document.querySelector('button[data-stop]');
-//   console.log(btnStart, btnStop);
+  let timerId = null;
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  function onStartClick() {
+    timerId = setInterval(
+      () => (document.body.style.backgroundColor = getRandomHexColor()),
+      1000
+    );
   
-//   btnStart.addEventListener('click', onBtnStartClick);
-//   btnStop.addEventListener('click', onBtnStopClick);
+    buttonStart.setAttribute('disabled', true);
+    buttonStop.removeAttribute('disabled');
+  }
   
-//   function onBtnStartClick() {
-//     timerId = setInterval(
-//       () => (document.body.style.backgroundColor = getRandomHexColor()),
-//       1000
-//     );
+  function onStopClick() {
+    buttonStop.setAttribute('disabled', true);
+    buttonStart.removeAttribute('disabled', false);
   
-//     btnStart.setAttribute('disabled', true);
-//     btnStop.removeAttribute('disabled');
-//   }
-  
-//   function onBtnStopClick() {
-//     btnStop.setAttribute('disabled', true);
-//     btnStart.removeAttribute('disabled', false);
-  
-//     clearInterval(timerId);
-//   }
+    clearInterval(timerId);
+  }
